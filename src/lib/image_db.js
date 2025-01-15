9afe28f612dd517da72e4c7d0ce8a1d3da5348cc
Murat Kirazkaya <groophylifefor@gmail.com>
2025-01-15 12:52:11 +0300

    storage init

--- src/lib/image_db.js
+++ src/lib/image_db.js
@@ -1,11 +1,27 @@
 import { Storage } from '@google-cloud/storage';
 
+export const getGCPCredentials = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return process.env.GCP_PRIVATE_KEY
+    ? {
+        credentials: {
+          client_email: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+          private_key: process.env.GCP_PRIVATE_KEY,
+        },
+        projectId: process.env.GCP_PROJECT_ID,
+      }
+    : {};
+  }
+
+  return {
+    projectId: process.env.GCLOUD_PROJECT_ID,
+  }
+};
+
+
 let storage = null;
 try {
-  storage = new Storage({
-    projectId: process.env.GCLOUD_PROJECT_ID,
-    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-  });
+  storage = new Storage(getGCPCredentials());
 } catch (err) {
   console.error('Storage initialize error', err);
   throw new Error('Storage initialize error');
@@ -13,8 +29,9 @@ try {
 
 let bucket = null;
 try {
-  bucket = storage.bucket(process.env.GCLOUD_BUCKET_NAME);
+  bucket = await storage.bucket(process.env.GCLOUD_BUCKET_NAME);
 } catch (err) {
+  console.warn('did you do Application Default Credentials (ADC) signup for local development?');
   console.error('Bucket initialize error', err);
   throw new Error('Bucket initialize error');
 }
@@ -23,12 +40,13 @@ async function saveImage(buffer, uid) {
   const uniqueName = `swiftbuddies-images/${uid}.png`;
 
   try {
-    bucket.file(uniqueName).save(buffer, {
+    await bucket.file(uniqueName).save(buffer, {
       contentType: 'image/png',
       gzip: true,
     });
     return true;
   } catch (err) {
+    console.warn('saveImage error', err);
     return false;
   }
 }
